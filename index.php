208ffb7e483c1ebd24fc7abb627f80d342a0e453
kimyh5392 <mk.kimyh@gmail.com>
2018-09-18 10:16:18 +0900

    마스터 리셋 셋팅 작업 완료. 서비스 항목 완료

---
+++ index.php
@@ -0,0 +1,170 @@
+<!doctype html>
+<html lang="ko">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>SRC 클리닉</title>
+		<link rel="stylesheet" href="/assets/fontello/css/fontello.css">
+		<link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="/assets/css/default.css">
+		<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+		<link rel="stylesheet" type="text/css" href="/assets/fullpage/fullpage.css" />
+		<!--[if IE 9]>
+            <link href="https://cdn.jsdelivr.net/gh/coliff/bootstrap-ie8/css/bootstrap-ie9.min.css" rel="stylesheet">
+        <![endif]-->
+        <!--[if lte IE 8]> 
+            <link href="https://cdn.jsdelivr.net/gh/coliff/bootstrap-ie8/css/bootstrap-ie8.min.css" rel="stylesheet">
+            <script src="https://cdn.jsdelivr.net/g/html5shiv@3.7.3"></script>
+        <![endif]-->
+	</head>
+	<body>
+		<header id="header">
+			<h1 class="logo">
+				<a href="/">
+					<img src="/assets/images/logo_white.png" class="logo-white" alt="로고">
+					<img src="/assets/images/logo_black.png" class="logo-black" alt="로고">
+				</a>
+			</h1>
+		</header>
+		<div id="fullpage">
+			<section class="section">
+				<!--jumbotrons slider -->
+                <a href="#" class="next-btn">
+                    <i class="icon-down-open-big">SCROLL</i>
+                </a>
+				<div class="jumbotron">
+					<ul class="slide-wrap">
+						<li>
+							<div class="summary">
+								<h2>
+									지난 60여년간 <br>
+									<strong>오직 한길만을 걸어온 <i>SRC병원</i></strong>
+								</h2>
+								<p>
+									건강한 삶과 일상으로의 복귀를 위해 함께 걸어가겠습니다. <br>지금부터 SRC만의 특화된 치료체계 및 환자 중심의 감동서비스를 경험하십시오.
+								</p>
+							</div>
+						</li>
+						<li>
+							<div class="summary">
+								<h2>
+									지난 60여년간 <br>
+									<strong>오직 한길만을 걸어온 <i>SRC병원</i></strong>
+								</h2>
+								<p>
+									건강한 삶과 일상으로의 복귀를 위해 함께 걸어가겠습니다. <br>지금부터 SRC만의 특화된 치료체계 및 환자 중심의 감동서비스를 경험하십시오.
+								</p>
+							</div>
+						</li>
+						<li>
+							<div class="summary">
+								<h2>
+									지난 60여년간 <br>
+									<strong>오직 한길만을 걸어온 <i>SRC병원</i></strong>
+								</h2>
+								<p>
+									건강한 삶과 일상으로의 복귀를 위해 함께 걸어가겠습니다. <br>지금부터 SRC만의 특화된 치료체계 및 환자 중심의 감동서비스를 경험하십시오.
+								</p>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</section>
+			<section class="section introduce">
+				<article class="germany">
+					<div class="details">
+						<small>독일 도수치료, 수술 후 재활치료, 비수술 척추 통증 치료</small>
+						<h3>
+							국내 유일 독일 도수의학<sup><img src="/assets/images/img_germany_sup.jpg" alt="독일국기"></sup><br>
+							<strong>
+								전문의가 직접 시행하는 <br>
+								도수치료
+							</strong>
+						</h3>
+						<div class="btn-area">
+							<p>
+								<a href="#" class="btn btn-default" role="button">통증 클리닉 바로가기</a>
+							</p> 
+						</div>
+					</div>
+				</article>		
+				<article class="rehabilitate">
+					<div class="details">
+						<small>소아재활 낮병동, 청소년 재활치료, 감각통합치료, 언어치료</small>
+						<h3>
+							우리 아이가<br>
+							<strong>아픔없이 환하게 웃는</strong><br>
+							그날을 위하여
+						</h3>
+						<div class="btn-area">
+							<p>
+								<a href="/rehabilitate/index.php" class="btn btn-default btn-rehabilitate" role="button">재활 클리닉 바로가기</a>
+							</p> 
+						</div>
+					</div>
+				</article>	
+			</section>
+		</div>
+		<footer id="footer">
+			<div class="container">
+				<div class="footer-info">
+					<p class="copyright">(C) 2018 SRC병원 부설의원. ALL RIGHT RESERVED. SITE BY MOVEMENT K GROUP.</p>
+					<div class="dropdown">
+						<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							FAMILY SITE
+						</button>
+						<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+							<a class="dropdown-item" href="#">site1</a>
+							<a class="dropdown-item" href="#">site2</a>
+							<a class="dropdown-item" href="#">site3</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+		<!-- Optional JavaScript -->
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+		<script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+		
+		<script src="/assets/bootstrap/js/bootstrap.min.js"></script>
+		<script src="/assets/fullpage/vendors/easings.min.js"></script>
+		<script src="/assets/fullpage/vendors/scrolloverflow.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.5/jquery.fullpage.js"></script>
+		<script>
+			(function($){
+				// full page 사용
+				$(document).ready(function() {
+                    $('#fullpage').fullpage({
+                        autoScrolling: true,
+                        scrollHorizontally: true
+                    });
+                });
+                
+                // introduce 효과
+                $("html, body").on("mousewheel DOMMouseScroll", function(e){
+                    $(".introduce").addClass("on");
+                });
+
+				// slick slider
+				$(document).ready(function(){ 
+					$('.jumbotron ul').slick({
+						dots: false,
+						infinite: true,
+						speed: 300,
+						fade: true,
+						autoplay: true,
+						cssEase: 'ease'
+						/*cssEase: 'linear'*/
+					});	
+				});
+				$('.jumbotron ul').on('init', function(event,slick){
+					setTimeout(function(){ 
+						$('.slick-slide').addClass('action'); 
+					});
+				});
+			})(jQuery);
+		</script>
+	</body>
+</html>
